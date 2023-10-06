7e02e48257c27b85af9f342354e83329887dd0b0
Isaac Hanes <isaacdhanes97@gmail.com>
2023-10-06 13:43:40 -0700

    additional eda in eda notebook, testing done in DFtool notebook, completed existing methods in wastateDFtool.py

--- src/wastateDFtool.py
+++ src/wastateDFtool.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+import argparse
+import os
+
+# setting a path variable to be used later
+mypath = "c:/Users/RGBMonster/galvanize/daimil10/case_studies/Mid_Term_Repos/Washington_State_Databreach_Assessment/images"
 
 # keeps pandas from displaying scientific notation
 pd.options.display.float_format = '{:.0f}'.format
@@ -37,7 +42,7 @@ class WaStateDBDF():
 
         wa_databreach_df = self.uncleandf 
         # drop unusable or unneccessary columns
-        wa_databreach_df = wa_databreach_df.drop(['Id', 'YearText', 'Year','WashingtoniansAffectedRange'],axis=1)
+        wa_databreach_df = wa_databreach_df.drop(['Id', 'YearText', 'Year','EndedOnDayDiscovered'],axis=1)
 
         # Scrub all instances where databreach cause is cyberattack and cyberattacktype is NaN, replace with 'Unreported'
         wa_databreach_df.loc[:, 'CyberattackType'][(wa_databreach_df['DataBreachCause'] == 'Cyberattack') 
@@ -48,24 +53,82 @@ class WaStateDBDF():
 
         return wa_databreach_df
 
-    def get_data_stats(self):
+    def get_data_stats(self, argument):
         '''
-        Grabs data statistics from self.cleandf and outputs them in a readable/usable format
-        '''
-        pass
+        Grabs data statistics from self.cleandf and outputs them in a readable/usable format.
 
+        Inputs: argument(use 'o' to output to images file, use 's' to output for viewing in jupyter notebook)
+
+        Outputs: a fig,axs object containing information about the table
+        '''
+        # help function
+        def get_nanscount_helpfunction(dataframe):
+            '''
+            Inputs: Dataframe
+
+            Outputs: A list of counts of nans per column
+            '''
+            countlist = []
+            colsarray = self.columnlist
+
+            for x in colsarray:
+                countlist.append(dataframe[f'{x}'].isnull().sum().sum())
+            return countlist
+
+        # help function
+        def get_uniquescount_helpfunction(dataframe):
+            '''
+            Inputs: Dataframe
+
+            Outputs: A list of counts of uniques per column
+            '''
+            countlist = []
+            colsarray = self.columnlist
+         
+            for x in colsarray:
+                countlist.append(len(dataframe[f'{x}'].unique()))
+            return countlist
+
+        # make a 2x2 fig,axs where each axs contains pertintent info about the dataframe
+        fig, axs = plt.subplots(2,1,figsize=(10,16))
+
+        fig.tight_layout()
+        
+        axs[0].barh(self.columnlist, get_nanscount_helpfunction(self.cleandf))
+        axs[0].set_title('Number of NaNs per Column')
+
+        axs[1].barh(self.columnlist, get_uniquescount_helpfunction(self.cleandf))
+        axs[1].set_title('Number of Unique Values per Column')
+        
+        if argument == 'o':
+            fig.savefig(f'{mypath}/data_stats', bbox_inches='tight')
+        
+        elif argument == 's':
+            plt.show()
+        
+        else:
+            plt.show()
+    
     def get_vcount(self, columname):
         '''
-        Gets pandas value count series from the given column name
+        Inputs: column name
+
+        Outputs: a pandas value count series object for the given column name
         '''
-        pass
+        return self.cleandf[columname].value_counts()
     
     def get_uniques(self, columname):
         '''
-        Gets numpy array of unique values from the given column name
+        Inputs: colum name
+
+        Outputs: numpy array of unique values from the given column name
         '''
-        pass
+        return self.cleandf[columname].unique()
 
 
 if __name__ == '__main__':
     Databreaches = WaStateDBDF('../data/wa_state_data_breaches.csv')
+
+    Databreaches.cleandf
+    Databreaches.columnlist
+    Databreaches.get_vcount('DiscoveredInProgress')
